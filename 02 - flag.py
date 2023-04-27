f6af220f707b84e15bc3614da05279d51f035877
IltonBJSilva <xlsonplay@gmail.com>
2023-04-27 00:09:38 -0300

    Demarcando variavel para saber se passou pela condicao

---
+++ 02 - flag.py
@@ -0,0 +1,27 @@
+"""
+
+Flag (Bandeira) - Marcar  um local
+Nonme = Não valor
+is e is not = é e não é (tipo, valor, identidade)
+id = Identidade
+
+"""
+
+condicao = True
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('Verdadeiro')
+else:
+    print('Falso')
+
+
+if  passou_no_if is None:
+    print('Não passou no if')
+else:
+    print('Passou no if')
+
+
+
+
