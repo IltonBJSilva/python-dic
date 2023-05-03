da63f6f626c95b9e389c9ad091acea420676347c
IltonBJSilva <xlsonplay@gmail.com>
2023-05-03 19:22:01 -0300

    Função de depositar termianda

--- Projetos/Sistema Bancario/main.py
+++ Projetos/Sistema Bancario/main.py
@@ -15,26 +15,41 @@ LIMITE_SAQUES = 3
 
 while True:
 
-    opcao = input(f"Escolha a opção: {menu}").upper()
+    try:
+        opcao = input(f"Escolha a opção: {menu}").upper()
+    except Exception as e:
+        print("Erro: ", e)
 
     if opcao == 'D':
         print("Depositar")
+        try:
+            valor = float(input("Digite o valor a ser depositado: "))
+        except Exception as e:
+            print("Erro: ", e)
+            continue
 
-        valor = float(input("Digite o valor a ser depositado: "))
+        if type(valor) != float:
+            print("Valor inválido")
+            continue
+        else:
+            saldo += valor
+            print(f"\nValor de R$ {valor:.2f} depositado com sucesso\n\nSeu saldo agora e de R$ {saldo:.2f}")
+
     elif opcao == "S":
         print("Sacar")
-        valor = float(input("Digite o valor a ser sacado: "))
+
+
 
     elif opcao == "e":
         print("Extrato")
 
     elif opcao == "Q":
         print("Sair")
+        print("Volte sempre")
+        print(f"Seu saldo final é de R$ {saldo:.2f}")
         break
-    
     else:
         print("Opção inválida")
-        continue
 
 
 
