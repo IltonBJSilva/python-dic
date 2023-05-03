1163a17ec534fa1c4a9c181bd5d2a4f56d7b984d
IltonBJSilva <xlsonplay@gmail.com>
2023-05-02 22:54:15 -0300

    primeira atividade do bootcamp de python da DIO

---
+++ 00 - atividade.py
@@ -0,0 +1,13 @@
+''' 
+IMPORTANTE: As funções "input" e "print" são acessíveis nativamente em Python, onde:  
+ - "input": função que permite a leitura de uma entrada do usuário. Lembre-se que, em alguns 
+   casos, é necessário converter/tratar os dados de entrada; 
+ - "print": função que imprime um texto enviado em seu parâmetro, a qual é essencial para a 
+   impressão dos dados de saída. 
+'''
+T = input("Digite um texto: ")
+
+if len(T) <= 140:   
+    print(f"TWEET")
+else:
+    print(f"MUTE")
